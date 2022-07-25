870bb557b19896984728a99563d4bca5903ed784
Simone Fortunato <frtsmn94@gmail.com>
2022-07-25 18:54:31 +0200

    Fine Prima Parte

--- js/main.js
+++ js/main.js
@@ -2,34 +2,35 @@ var app= new Vue(
     {
         el:'#app',
         data:{
-            nuovoTask:'',
-            arrayTask:[
-                       
-
-
-                     ]
+            nuovoTask: "",
+            arrayTask:[]
         },
+
+
         methods:{
             aggiungitask(){
+                if(this.nuovoTask == ""){
+                    alert('il campo nuovo task non può essere vuoto');
+                }else{
                 this.arrayTask.push({task: this.nuovoTask, stato: true});
                 console.log(this.arrayTask)
-                this.nuovoTask=" ";
+                this.nuovoTask="";
                 console.log(this.nuovoTask)
+
+            }
             },
 
             eliminatask(indice){
                 this.arrayTask.splice(indice,1);
             },
 
-            completato(){
-                    this.arrayTask.Task.stato == false;
-                
+            completato(indice){
+               this.arrayTask[indice].stato = true;
+                console.log(this.arrayTask[indice].stato)
+               
+               
             }
-
-        
         },
-       
     }
-   
 );
 
