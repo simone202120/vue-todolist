27f07e63b1b3dc3bc4a0199a84ee95019387a1c5
Simone Fortunato <frtsmn94@gmail.com>
2022-07-25 17:00:58 +0200

    Parte 1

--- js/main.js
+++ js/main.js
@@ -3,16 +3,27 @@ var app= new Vue(
         el:'#app',
         data:{
             nuovoTask:'',
-            arrayTask:[]
+            arrayTask:[
+                       
+
+
+                     ]
         },
         methods:{
             aggiungitask(){
                 this.arrayTask.push({task: this.nuovoTask, stato: true});
                 console.log(this.arrayTask)
+                this.nuovoTask=" ";
+                console.log(this.nuovoTask)
             },
 
-            eliminatask(){
+            eliminatask(indice){
+                this.arrayTask.splice(indice,1);
+            },
 
+            completato(){
+                    this.arrayTask.Task.stato == false;
+                
             }
 
         
